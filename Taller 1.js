258934e8a1a1ba3f3e421dbd897958690b7c8971
Andres Gomez <andres_felipe1822@hotmail.com>
2023-03-06 17:07:45 -0500

    Taller 1 v1.0.0

---
+++ Taller 1.js
@@ -0,0 +1,24 @@
+//Definir 2 variables, asignarles un valor y con if imprimer el que tenga menor valor
+
+var a=10;
+var b=20;
+
+if (a<b){
+    console.log(a + " es el menor")
+}
+else{
+    console.log(b + " es menor")
+}
+
+//definir 3 variables tres variables, asignarles un valor y imprima el que tenga mayor valor
+var c=3, d=4, e=5;
+
+if (c> d && e){
+    console.log(c)
+}
+else if(d> e && c){
+    console.log(d)
+}
+else{
+    console.log(e)
+}
